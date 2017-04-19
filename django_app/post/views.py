aa2c182ec55606a8151c7efb214e91c7c4d57f1f
starsol89 <sinabro1143@gmail.com>
2017-04-19 12:57:51 +0900

    Views.py 와 config 메인 urls.py post 주소 추가

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,26 +1,27 @@
-# import django_filters
-# from django.contrib.auth import get_user_model
-# from rest_framework import viewsets, permissions
-#
-# from post.models import Post
-# from .serializers import PostSerializer
-#
-# User = get_user_model()
-#
-#
-# class PostFilter(django_filters.rest_framework.FilterSet):
-#     year = django_filters.Numberfilter(name='day', lookup_expr='year')
-#     month = django_filters.Numberfilter(name='day', lookup_expr='month')
-#
-#     class Meta:
-#         model = Post
-#         fields = ('year', 'month',)
-#
-#
-# class PostViewSet(viewsets.ModelViewSet):
-#     serializer_class = PostSerializer
-#     filter_class = PostFilter
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-#
-#     def get_queryset(self):
-#         return Post.objects.filter(author=self.request.user)
+import django_filters
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, permissions
+
+from post.models import Post
+from .serializers import PostSerializer
+
+User = get_user_model()
+
+
+class PostFilter(django_filters.rest_framework.FilterSet):
+    year = django_filters.NumberFilter(name='day', lookup_expr='year')
+    month = django_filters.NumberFilter(name='day', lookup_expr='month')
+
+    class Meta:
+        model = Post
+        fields = ('year', 'month',)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_class = PostFilter
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
